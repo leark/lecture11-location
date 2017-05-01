55efd977d5232a49d131b2124492572154eeba76
Joel Ross <joelross@uw.edu>
2017-05-01 11:27:09 -0700

    Reset to starter code

--- app/src/main/java/edu/uw/locationdemo/MainActivity.java
+++ app/src/main/java/edu/uw/locationdemo/MainActivity.java
@@ -1,31 +1,12 @@
 package edu.uw.locationdemo;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LOCATION";
-
-    private static final int LOCATION_REQUEST_CODE = 1;
-
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
 
     private TextView textLat;
     private TextView textLng;
@@ -38,80 +19,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         textLat = (TextView)findViewById(R.id.txt_lat);
         textLng = (TextView)findViewById(R.id.txt_lng);
 
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.v(TAG, "GoogleApiClient connected");
-
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            //have permission, can go ahead and do stuff
-
-            //assumes location settings enabled
-
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        }
-        else {
-            //request permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
 
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if(location != null) {
-            textLat.setText("" + location.getLatitude());
-            textLng.setText("" + location.getLongitude());
-        }
-        else {
-            Log.v(TAG, "Received null location");
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch(requestCode){
-            case LOCATION_REQUEST_CODE: { //if asked for location
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    onConnected(null); //do whatever we'd do when first connecting (try again)
-                }
-            }
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
 }
