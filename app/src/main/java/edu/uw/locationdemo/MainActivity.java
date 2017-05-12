f0dd4b855a52a5ea4868981cd27050c84624c17a
Seung Lee <shl7@uw.edu>
2017-05-11 18:01:27 -0700

    Added lecture code

--- app/src/main/java/edu/uw/locationdemo/MainActivity.java
+++ app/src/main/java/edu/uw/locationdemo/MainActivity.java
@@ -1,15 +1,31 @@
 package edu.uw.locationdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "LOCATION";
+    private static final int LOC_RQUEST_CODE = 1;
 
     private TextView textLat;
     private TextView textLng;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +35,67 @@ public class MainActivity extends AppCompatActivity {
         textLat = (TextView)findViewById(R.id.txt_lat);
         textLng = (TextView)findViewById(R.id.txt_lng);
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // Ready to get location
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            // okay, send request!
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOC_RQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // called when location changed
+        if (location != null) {
+            textLat.setText("" + location.getLatitude());
+            textLng.setText("" + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOC_RQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onConnected(null);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
